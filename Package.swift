b44535ce6b8d1b9b6a84e36f51fab6b1f9395e15
James Straub <jamsajones@users.noreply.github.com>
2024-03-19 19:19:15 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,16 +12,4 @@ let package = Package(
     dependencies: [
         // This package has no external dependencies.
     ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "RangeSeekSlider",
-            dependencies: [],
-            path: "Source"), // Adjust the path as necessary
-        .testTarget(
-            name: "RangeSeekSliderTests",
-            dependencies: ["RangeSeekSlider"],
-            path: "RangeSeekSliderTests"), // Adjust the path as necessary
-    ]
 )
