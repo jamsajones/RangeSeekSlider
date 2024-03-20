dd44cd552716f2327b95736e91f4ce23781ee4de
James Straub <jamsajones@users.noreply.github.com>
2024-03-19 19:28:43 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
         .iOS(.v9),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RangeSeekSlider",
             targets: ["RangeSeekSlider"]),
