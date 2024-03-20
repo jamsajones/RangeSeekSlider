a7bd51613e9982c850217a9f283d1addb8c66f73
James Straub <jamsajones@users.noreply.github.com>
2024-03-19 19:24:38 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,11 +3,26 @@ import PackageDescription
 
 let package = Package(
     name: "RangeSeekSlider",
+    platform: [
+        .iOS(.v9),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "RangeSeekSlider")
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]),
     ],
     dependencies: [
         // This package has no external dependencies.
+    ],
+    targets: [
+        .target(
+            name: "RangeSeekSlider",
+            dependencies: [],
+            path: "Sources"), 
+        .testTarget(
+            name: "RangeSeekSliderTests",
+            dependencies: ["RangeSeekSlider"],
+            path: "Tests"), // Adjust the path as necessary
     ]
 )
